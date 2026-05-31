64ee6d6ab7470100d4618e0633c4f20677f6a590
kasinpod-40 <kasinpod40@gmail.com>
2026-06-01 02:21:50 +0700

    Update score.js

--- src/utils/score.js
+++ src/utils/score.js
@@ -1,11 +1,12 @@
 export function calculateLeadScore(ai) {
   let score = 0
-
-  if (ai.interest_level === "high") score += 50
-
-  if (ai.hot_lead) score += 30
-
-  if (ai.customer_stage === "closing") score += 20
+  if (ai.customer_stage === "customer") {
+    score == 100
+  } else {
+    if (ai.interest_level === "high") score += 40
+    if (ai.hot_lead) score += 30
+    if (ai.customer_stage === "closing") score += 20
+  }
 
   return Math.min(score, 100)
 }
